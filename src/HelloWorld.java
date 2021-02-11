560ac50095cdb462a8148321b175e33fa1145bbf
AnimalisticBeast <dave.walsh@live.com>
2021-02-10 21:45:53 -0500

    is this ok,

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,7 @@
 public class HelloWorld {
     public static void main(String[]args){
         System.out.println("Hello World");
-        System.out.println("hi GBank!!!");
-        System.out.println("Dave is a nerd");
-        System.out.println("BIG TIME NERD");
-
+        //Is this right
+        
     }
 }
