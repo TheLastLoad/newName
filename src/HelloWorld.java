f69aec35ac18ea792d689283662375f2c5d1edec
Kyle <dominy.kyle@gmail.com>
2021-02-10 20:59:53 -0500

    Dave is a nerd

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
     public static void main(String[]args){
         System.out.println("Hello World");
         System.out.println("hi GBank!!!");
+        System.out.println("Dave is a nerd");
+        System.out.println("BIG TIME NERD");
     }
 }
